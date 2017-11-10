1d5b35578dc610181671394251af041b5e96da5a
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-11-10 19:49:34 +0530

    readings

--- src/main/scala/com/weatherSimulator/temperature.scala
+++ src/main/scala/com/weatherSimulator/temperature.scala
@@ -2,4 +2,12 @@ package com.weatherSimulator
 
 object temperature {
   
+  def readings(minTemp: Int, maxTemp: Int):Int= {
+    
+    val temp = minTemp + Randowm.nextInt(maxTemp-minTemp)
+    
+    temp
+    
+  }
+  
 }
