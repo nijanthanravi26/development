c3a6e31cb3bd1cd080e4871c0b97f43197b1973e
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-11-10 22:28:52 +0530

    iteration

--- src/main/scala/com/weatherSimulator/getData.scala
+++ src/main/scala/com/weatherSimulator/getData.scala
@@ -4,27 +4,25 @@ import scala.util.Random
 object getData {
   
   def main(args: Array[String]): Unit = {
+    
+    val iteration=args(0)
+    
+    for( i <- 0 until iteration)
+    {
   
-      val (place, location) = locationData.cities(Random.nextInt(locationData.cities.length))
-      val (latitude, longitude, elevation) = location
-      
-      val timestamp=generateTimestamp.dateTime(2010,2020)
-//      val dateFormat = datetime.split(",")
-//      val date = dateFormat(0)
-//      val month = dateFormat(1)
-//      val year = dateFormat(2)
-//      val hms = dateFormat(3)
-//      
-//      val timestamp = date+"-"+month+"-"+year" "+hms
-      
-      
-      val temp=temperature.readings(-45,45)
-      val con = weatherCondition.getCondition(temp)
-      val pressure=pressureGen.data(800,1200)
-      val humidity=humidityGen.data(10,100)
-      
-    println(s"$place,$latitude,$longitude,$elevation,$timestamp,$temp,$con,$pressure,$humidity")
-    //println(place,latitude,longitude,elevation,date,temp)
+          val (place, location) = locationData.cities(Random.nextInt(locationData.cities.length))
+          val (latitude, longitude, elevation) = location
+          
+          val timestamp=generateTimestamp.dateTime(2010,2020)
+        
+          
+          val temp=temperature.readings(-45,45)
+          val con = weatherCondition.getCondition(temp)
+          val pressure=pressureGen.data(800,1200)
+          val humidity=humidityGen.data(10,100)
+          
+          println(s"$place,$latitude,$longitude,$elevation,$timestamp,$temp,$con,$pressure,$humidity")
+    }
   
 }
 }
