b86281e4499f4f4bc636a7562bbd8c411599d741
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-11-10 20:49:17 +0530

    status

--- src/main/scala/com/weatherSimulator/weatherCondition.scala
+++ src/main/scala/com/weatherSimulator/weatherCondition.scala
@@ -3,13 +3,17 @@ package com.weatherSimulator
 object weatherCondition {
   
   def getCondition(temperature: Int):String = {
+    
+    var status = null.toString
+    
     if (temperature > 0 && temperature < 15) {
-      println("Rainy")
+      status="Rainy"
     } else if (temperature > 15 && temperature < 45) {
-      println("HoT") 
+      status="HoT" 
     } else {
-      println("Snow") 
+      status="Snow" 
     }
+    status
   }
   
 }
