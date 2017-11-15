71abc9d291cbca729505566771990353c2a269dc
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-11-15 23:40:12 +0530

    syntax err

--- src/main/scala/com/weatherSimulator/humidityGen.scala
+++ src/main/scala/com/weatherSimulator/humidityGen.scala
@@ -17,8 +17,8 @@ object humidityGen {
   
   def formula(airTemperature:Int):Double={
     
-    val satVaporPressure=6.11 * Math.pow(10.0,(7.5*airTemperature/(237.7+airTemperature))
-    val actVaporPressure=6.11 * math.pow(10.0,(7.5*dewPointTemperature/(237.7+dewPointTemperature))
+    val satVaporPressure=6.11 * Math.pow(10.0,(7.5*airTemperature/(237.7+airTemperature)))
+    val actVaporPressure=6.11 * math.pow(10.0,(7.5*dewPointTemperature/(237.7+dewPointTemperature)))
     val relativeHumidity=(actVaporPressure/satVaporPressure)*100
     relativeHumidity
   }
