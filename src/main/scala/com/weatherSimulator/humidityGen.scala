d9f0bb6f87bd766103b3c8ed98a5d2deb5c8083d
Nijanthan26 <nijanthan.ravi@antuit.com>
2017-11-15 23:45:52 +0530

    final comment

--- src/main/scala/com/weatherSimulator/humidityGen.scala
+++ src/main/scala/com/weatherSimulator/humidityGen.scala
@@ -2,6 +2,8 @@ package com.weatherSimulator
 
 import scala.util.Random
 
+//Ref : http://www.wral.com/how-to-calculate-relative-humidity/1174528/
+
 object humidityGen {
   
  
@@ -9,6 +11,7 @@ object humidityGen {
   
   def formula(airTemperature:Int):Double={
     
+    //Calculate relative humidity using dew point temperature and air temperature.
     val satVaporPressure=6.11 * Math.pow(10.0,(7.5*airTemperature/(237.7+airTemperature)))
     val actVaporPressure=6.11 * math.pow(10.0,(7.5*dewPointTemperature/(237.7+dewPointTemperature)))
     val relativeHumidity=(actVaporPressure/satVaporPressure)*100
